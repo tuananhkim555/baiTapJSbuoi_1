e9a2d47346a2919bda906d345fa0f9928383a26a
“tuananhkim555” <tuananhkim555@gmail.com>
2024-06-27 00:17:55 +0700

    baitapJs_buoi1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './App.css'
+import { Navbar } from './components/Navbar.jsx'
+import { Header } from './components/header.jsx'
+// eslint-disable-next-line no-unused-vars
+import { TabSeciton } from './components/TabSection.jsx'
+import { Footer } from './components/Footer.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <TabSeciton />
+      <Footer />
+    </>
+   
+
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer(){
+    return(
+        <>
+             <footer className="py-5 bg-dark">
+            <div className="container"><p className="m-0 text-center text-white">Copyright © Your Website 2023</p></div>
+            </footer>
+
+        </>
+    )
+}
